787158b940cee2d5b934d1d7b102ea60df60e1cb
Vi Thi Lan <lanvt6596@ut.edu.vn>
2025-03-30 20:41:30 +0700

    commit

--- configs/database.php
+++ configs/database.php
@@ -3,5 +3,6 @@ $config['database'] = [
     'db_host' => 'localhost',
     'user' => 'root',
     'pass' => '',
-    'db_name' => 'van_phong_pham'
+    'db_name' => 'van_phong_pham',
+    'port' => 3307
 ];
--- core/Connection.php
+++ core/Connection.php
@@ -8,7 +8,7 @@ class Connection
     private function __construct($config)
     {
         //        Kết nối database
-        $this->conn = new mysqli($config['db_host'], $config['user'], $config['pass'], $config['db_name']);
+        $this->conn = new mysqli($config['db_host'], $config['user'], $config['pass'], $config['db_name'], $config['port']);
 
         // if ($this->conn->connect_error) {
         //     Logger::logError("Connection failed: " . $this->conn->connect_error);
