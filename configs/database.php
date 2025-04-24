125cba8c1490fa547851fb290e303461cf7e4103
Tuyet Sang <sanghtt1001@ut.edu.vn>
2025-04-24 16:12:18 +0700

    m

--- configs/database.php
+++ configs/database.php
@@ -2,13 +2,8 @@
 $config['database'] = [
     'db_host' => 'localhost',
     'user' => 'root',
-<<<<<<< HEAD
-    'pass' => '27032005',
-    'db_name' => 'van_phong_pham'
-=======
     'pass' => '',
     'db_name' => 'van_phong_pham',
 
->>>>>>> ef31d35316b5bcebcf840fb700d440ba8d6421c5
 ];
 ?>
