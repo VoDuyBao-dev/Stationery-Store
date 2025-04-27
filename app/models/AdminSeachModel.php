4d212b8514fdf108b27aa0c5bb8d68093a04a748
Vi Thi Lan <lanvt6596@ut.edu.vn>
2025-04-28 02:36:59 +0700

    xong tim kiem admin

--- app/models/AdminSeachModel.php
+++ app/models/AdminSeachModel.php
@@ -1,6 +1,6 @@
 <?php
 
-class AdminSearchModel extends Model
+class AdminSeachModel extends Model
 {
     public function search($keyword)
     {
@@ -13,21 +13,23 @@ class AdminSearchModel extends Model
 
         $searchKey = '%' . $keyword . '%';
 
+
         // Tìm kiếm sản phẩm
-        $sqlProduct = "SELECT product_id, name, price FROM products WHERE name LIKE ?";
-        $result['products'] = $this->fetchAll($sqlProduct, [$keyword]);
+        $sqlProduct = "SELECT products.product_id, products.name from products WHERE products.name LIKE ? or  products.product_id LIKE ? limit 10";
+        $result['products'] = $this->fetchAll($sqlProduct, [$searchKey, $searchKey]);
+
 
         // Tìm kiếm tài khoản người dùng
         $sqlUser = "SELECT user_id, fullname, email FROM users WHERE fullname LIKE ? OR email LIKE ?";
-        $result['users'] = $this->fetchAll($sqlUser, [$keyword, $keyword]);
+        $result['users'] = $this->fetchAll($sqlUser, [$searchKey, $searchKey]);
 
         // Tìm kiếm đơn hàng
-        $sqlOrder = "SELECT order_id, order, user_id, status FROM orders WHERE order_id LIKE ?";
-        $result['orders'] = $this->fetchAll($sqlOrder, [$keyword]);
+        $sqlOrder = "SELECT order_id, total_price, trangThaiGiao FROM orders WHERE order_id LIKE ?";
+        $result['orders'] = $this->fetchAll($sqlOrder, [$searchKey]);
 
         // Tìm kiếm mã giảm giá
-        $sqlCoupon = "SELECT coupon_id, code, discount_value FROM coupons WHERE code LIKE ?";
-        $result['coupons'] = $this->fetchAll($sqlCoupon, [$keyword]);
+        $sqlCoupon = "SELECT coupon_id, code, discount FROM coupons WHERE code LIKE ?";
+        $result['coupons'] = $this->fetchAll($sqlCoupon, [$searchKey]);
 
         return $result;
     }
